7ffe8216ec0a5a978b3e714543d295bc426126d0
Eeshi Uppalapati <euppalapati@ucsd.edu>
2025-01-23 21:26:42 -0800

    step 3

--- global.js
+++ global.js
@@ -4,10 +4,38 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a")
+// const navLinks = $$("nav a")
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-  );
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
 
-currentLink?.classList.add('current');
+// currentLink?.classList.add('current');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'cv/', title: 'CV' },
+    { url: 'https://github.com/euppalapati', title: 'GitHub' },
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+    let url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    let title = p.title;
+    
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+      }
+      
+    nav.append(a);
+  }
