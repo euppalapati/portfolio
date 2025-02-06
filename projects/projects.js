2925695afc962f9497bd86a86444c821dffeb3e5
Eeshi Uppalapati <euppalapati@ucsd.edu>
2025-02-05 19:12:57 -0800

    step 3

--- projects/projects.js
+++ projects/projects.js
@@ -19,33 +19,45 @@ let arc = arcGenerator({
 
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
-let data = [
-    { value: 1, label: 'apples' },
-    { value: 2, label: 'oranges' },
-    { value: 3, label: 'mangos' },
-    { value: 4, label: 'pears' },
-    { value: 5, label: 'limes' },
-    { value: 5, label: 'cherries' },
-  ];
-let sliceGenerator = d3.pie().value((d) => d.value);
-let arcData = sliceGenerator(data);
-let arcs = arcData.map((d) => arcGenerator(d));
-let colors = d3.scaleOrdinal(d3.schemeTableau10);
+// let data = [
+//     { value: 1, label: 'apples' },
+//     { value: 2, label: 'oranges' },
+//     { value: 3, label: 'mangos' },
+//     { value: 4, label: 'pears' },
+//     { value: 5, label: 'limes' },
+//     { value: 5, label: 'cherries' },
+//   ];
 
-arcs.forEach((arc, idx) => {
-    d3.select('svg')
-    .append('path')
-    .attr('d', arc)
-    .attr('fill', colors(idx));
+let projects = await fetchJSON('../lib/projects.json'); // fetch your project data
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year,
+);
+
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
 });
 
-let legend = d3.select('.legend');
-data.forEach((d, idx) => {
-    legend.append('li')
-        .attr('style', `--color:${colors(idx)}`)
-        .attr('class', 'legend-item')
-        .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
-});
+  let sliceGenerator = d3.pie().value((d) => d.value);
+  let arcData = sliceGenerator(data);
+  let arcs = arcData.map((d) => arcGenerator(d));
+  let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+  arcs.forEach((arc, idx) => {
+      d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', colors(idx));
+  });
+
+  let legend = d3.select('.legend');
+  data.forEach((d, idx) => {
+      legend.append('li')
+          .attr('style', `--color:${colors(idx)}`)
+          .attr('class', 'legend-item')
+          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+  });
 
 window.addEventListener("resize", function () {
     let width = Math.min(window.innerWidth * 0.4, 300);
